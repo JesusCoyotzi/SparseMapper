2363ade69ffef560fb1ecf59ee97325ffc9f2ab1
JesusCoyotzi <jesus_coyotzi@hotmail.com>
2022-05-10 00:13:01 -0500

    worked around separable compilation not working

--- space_quantization/src/gpu/operations.cu
+++ space_quantization/src/gpu/operations.cu
@@ -40,23 +40,3 @@ void printPoint3Array(point3 *p, int n)
         }
 }
 
-__device__ __host__ point3 addPoint3(point3 p1,point3 p2)
-{
-        point3 sum;
-        sum.x=p1.x+p2.x;
-        sum.y=p1.y+p2.y;
-        sum.z=p1.z+p2.z;
-
-        return sum;
-}
-
-__device__ __host__ point3 mulPoint3(point3 p, float s)
-{
-        point3 mul;
-        mul.x=s*p.x;
-        mul.y=s*p.y;
-        mul.z=s*p.z;
-
-        return mul;
-}
-
--- space_quantization/src/gpu/quantization.cu
+++ space_quantization/src/gpu/quantization.cu
@@ -7,6 +7,27 @@ __device__ __host__ float euclideanDistance(point3 p1, point3 p2)
         return ( (p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y)+(p1.z-p2.z)*(p1.z-p2.z));
 }
 
+__device__ __host__ point3 addPoint3(point3 p1,point3 p2)
+{
+        point3 sum;
+        sum.x=p1.x+p2.x;
+        sum.y=p1.y+p2.y;
+        sum.z=p1.z+p2.z;
+
+        return sum;
+}
+
+__device__ __host__ point3 mulPoint3(point3 p, float s)
+{
+        point3 mul;
+        mul.x=s*p.x;
+        mul.y=s*p.y;
+        mul.z=s*p.z;
+
+        return mul;
+}
+
+
 point3 randomPoint3()
 {
         float x = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
